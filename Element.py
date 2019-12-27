c09badaded24e31337cb71287d394824cf8dfde7
Agata Pietrowska <48550541+agatapietri@users.noreply.github.com>
2019-12-27 16:07:08 +0100

    mes

---
+++ Element.py
@@ -0,0 +1,10 @@
+import Node
+
+
+class Element:
+    def __init__(self, nodes: [Node], id):
+        self.nodes = nodes
+        self.id = id
+
+
+
---
+++ Node.py
@@ -0,0 +1,6 @@
+class Node:
+    def _init_(self, x, y, boundary_condition, id):
+        self.x = x
+        self.y = y
+        self.boundary_condition = boundary_condition
+        self.id = id
