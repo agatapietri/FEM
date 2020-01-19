ea813a647c0ec0bb992cfd70e0ef4e4311313014
Agata Pietrowska <48550541+agatapietri@users.noreply.github.com>
2020-01-19 20:16:17 +0100

    added loading data from json

--- Grid.py
+++ Grid.py
@@ -3,6 +3,9 @@ from Node import Node
 from numpy import arange
 from UniversalElement import UniversalElement
 import numpy as np
+import json
+
+
 class Grid:
     def __init__(self, H, W, nH, nW):
         self.nodes = []
@@ -14,7 +17,6 @@ class Grid:
         elementHeight = H / (nH - 1)
 
         node_id = 1
-
         for x in arange(0, W + elementWidth, elementWidth):
             for y in arange(0, H + elementHeight, elementHeight):
                 new_node = Node(x, y, None, None, node_id)
@@ -96,7 +98,8 @@ class Grid:
 
         return global_P_vector
 
-    def simulation(self, initial_temp, simulation_time, sim_time_step, universal_element, alpha, conductivity, density, specific_heat):
+    def simulation(self, universal_element, initial_temp, simulation_time, sim_time_step,ambient_temp,
+                   alpha, specific_heat, conductivity, density):
         t0_Vector = [initial_temp for i in range(self.nN)]
         t0_Vector = np.asanyarray([t0_Vector]).reshape((self.nN,1))
         step_amount = (int)(simulation_time/sim_time_step)
@@ -106,7 +109,7 @@ class Grid:
 
             global_h = self.H_matrix_global(universal_element, conductivity)
             global_c = self.C_matrix_global(universal_element, density, specific_heat)
-            p_vector_global = self.P_vector_global(universal_element, alpha, 1200)
+            p_vector_global = self.P_vector_global(universal_element, alpha, ambient_temp)
             global_h_bc = self.H_BC_global(universal_element, alpha)
             global_h += global_h_bc
             global_c *= 1/sim_time_step
@@ -125,17 +128,25 @@ class Grid:
 
 
 if __name__ == "__main__":
-    test = Grid(0.1, 0.1, 4, 4)
+
+    with open('MES.json', 'r') as data_file:
+        mes = json.load(data_file)
+    test = Grid(mes["H"], mes["W"], mes["N_H"], mes["N_W"])
+    P_vector_glogal = test.P_vector_global(UniversalElement(), 300, 1200)
+    print(P_vector_glogal)
+    print("------------------------------------------------------")
     H_global_test = test.H_matrix_global(UniversalElement( ), 30)
     print(H_global_test)
     C_global_test = test.C_matrix_global(UniversalElement( ), 7800, 700)
     print(C_global_test)
     print("------------------------------------------------------")
-    H_BC_global_test = test.H_BC_global(UniversalElement( ),25)
+    H_BC_global_test = test.H_BC_global(UniversalElement( ), 25)
     print(H_global_test)
     print("------------------------------------------------------")
     P_vector_test = test.P_vector_global(UniversalElement( ), 25, 1000)
     print(P_vector_test)
     print("------------------------------------------------------")
-    simulation_test = test.simulation(100, 500, 50, UniversalElement(), 300, 25, 7800, 700 )
+    simulation_test = test.simulation(UniversalElement(),mes["initial_temp"], mes["simulation_time"],
+                                      mes["step_time"], mes["ambient_temp"], mes["alpha"],
+                                      mes["specific_heat"], mes["conductivity"], mes["density"])
     print(simulation_test)
